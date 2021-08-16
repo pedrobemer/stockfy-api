796c3ba52c1a079eaf00591e5f3020d59f22f9ff
PedroBemerguy <pedrobemerguy96@gmail.com>
2021-08-16 20:56:07 -0300

    Update order_test file to include all the unit test from order.go

--- database/order_test.go
+++ database/order_test.go
@@ -77,8 +77,8 @@ func TestCreateOrder(t *testing.T) {
 	mock.ExpectBegin()
 	// mock.ExpectRollback()
 	rows := mock.NewRows(columns)
-	mock.ExpectQuery(insertRow).WithArgs(10.0, 20.29, "USD", "buy", "0001-01-01 00:00:00 +0000 UTC",
-		"1111BBBB-ed8b-11eb-9a03-0242ac130003",
+	mock.ExpectQuery(insertRow).WithArgs(10.0, 20.29, "USD", "buy",
+		"0001-01-01 00:00:00 +0000 UTC", "1111BBBB-ed8b-11eb-9a03-0242ac130003",
 		"55555555-ed8b-11eb-9a03-0242ac130003").
 		WillReturnRows(rows.AddRow("a8a8a8a8-ed8b-11eb-9a03-0242ac130003", 10.0,
 			20.29, "USD", "buy", tr, &brokerageInfo))
@@ -93,4 +93,137 @@ func TestCreateOrder(t *testing.T) {
 
 	assert.NotNil(t, orderReturn)
 	assert.Equal(t, expectedOrderReturn, orderReturn)
+}
+
+func TestDeleteOrder(t *testing.T) {
+
+	expectedOrderId := "a8a8a8a8-ed8b-11eb-9a03-0242ac130003"
+
+	query := regexp.QuoteMeta(`
+	delete from orders as o
+	where o.id = $1
+	returning o.id
+	`)
+
+	columns := []string{"id"}
+
+	mock, err := pgxmock.NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mock.Close(context.Background())
+
+	rows := mock.NewRows(columns)
+	mock.ExpectQuery(query).WithArgs("a8a8a8a8-ed8b-11eb-9a03-0242ac130003").
+		WillReturnRows(rows.AddRow("a8a8a8a8-ed8b-11eb-9a03-0242ac130003"))
+
+	orderId := DeleteOrder(mock, "a8a8a8a8-ed8b-11eb-9a03-0242ac130003")
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	assert.NotNil(t, orderId)
+	assert.Equal(t, expectedOrderId, orderId)
+}
+
+func TestDeleteOrders(t *testing.T) {
+
+	expectedOrderIds := []OrderApiReturn{
+		{
+			Id: "a8a8a8a8-ed8b-11eb-9a03-0242ac130003",
+		},
+		{
+			Id: "b7a8a8a8-ed8b-11eb-9a03-0242ac130003",
+		},
+	}
+
+	queryDeleteOrders := regexp.QuoteMeta(`
+	delete from orders as o
+	where o.asset_id = $1
+	returning o.id;
+	`)
+
+	columns := []string{"id"}
+
+	mock, err := pgxmock.NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mock.Close(context.Background())
+
+	rows := mock.NewRows(columns)
+	mock.ExpectQuery(queryDeleteOrders).WithArgs("ITUB4").
+		WillReturnRows(rows.AddRow("a8a8a8a8-ed8b-11eb-9a03-0242ac130003").
+			AddRow("b7a8a8a8-ed8b-11eb-9a03-0242ac130003"))
+
+	orderIds := DeleteOrders(mock, "ITUB4")
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	assert.NotNil(t, orderIds)
+	assert.Equal(t, expectedOrderIds, orderIds)
+}
+
+func TestUpdateOrder(t *testing.T) {
+	tr, err := time.Parse("2021-07-05", "2020-04-02")
+
+	orderInsert := OrderBodyPost{
+		Id:        "3e3e3e3w-ed8b-11eb-9a03-0242ac130003",
+		Symbol:    "VTI",
+		Fullname:  "Vanguard Total Stock Market US",
+		Brokerage: "Avenue",
+		Quantity:  20.0,
+		Price:     20.29,
+		Currency:  "USD",
+		OrderType: "buy",
+		Date:      "0001-01-01 00:00:00 +0000 UTC",
+		Country:   "US",
+		AssetType: "ETF",
+	}
+
+	expectedUpdatedOrder := []OrderApiReturn{
+		{
+			Id:        "3e3e3e3w-ed8b-11eb-9a03-0242ac130003",
+			Quantity:  20.0,
+			Price:     20.29,
+			Date:      tr,
+			OrderType: "buy",
+		},
+	}
+
+	query := regexp.QuoteMeta(`
+	update orders as o
+	set quantity = $2,
+		price = $3,
+		order_type = $4,
+		"date" = $5
+	where o.id = $1
+	returning o.id, o.quantity, o.price, o."date", o.order_type;
+	`)
+
+	columns := []string{"id", "quantity", "price", "date", "order_type"}
+
+	mock, err := pgxmock.NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mock.Close(context.Background())
+
+	rows := mock.NewRows(columns)
+	mock.ExpectQuery(query).WithArgs("3e3e3e3w-ed8b-11eb-9a03-0242ac130003",
+		20.0, 20.29, "buy", "0001-01-01 00:00:00 +0000 UTC").WillReturnRows(
+		rows.AddRow("3e3e3e3w-ed8b-11eb-9a03-0242ac130003", 20.0, 20.29,
+			tr, "buy"))
+
+	updatedOrder := UpdateOrder(mock, orderInsert)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	assert.NotNil(t, updatedOrder)
+	assert.Equal(t, expectedUpdatedOrder, updatedOrder)
 }
